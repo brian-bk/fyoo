9013af54b0aeb17c84377095f2aef1dfa99ceb76
Brian Kleszyk <brian@teacherspayteachers.com>
2022-08-01 22:38:49 -0400

    Drop support for 3.6 as it is EOL

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ def do_setup():
                 'fyoo = fyoo.cli:main',
             ],
         },
-        python_requires='>=3.6',
+        python_requires='>=3.7',
         cmdclass={
             'verify': VerifyVersionCommand,
             'show_install': ShowInstallExtrasCommand,
@@ -125,9 +125,10 @@ def do_setup():
         classifiers=[
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
         ],
     )
 
